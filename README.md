b173cd1e25b9049c44c10d3f2235f9735c09546a
Johnny Mnemonic <jm@machine-hall.org>
2014-12-07 18:10:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# NFS root file system builder #
+
+The tools in this repo allow to create NFS root file systems for OpenBSD from an OpenBSD host automatically.
+
+## Prerequisites ##
+
+In addition to a default OpenBSD installation you need:
+
+* bash
+* wget
+
+## How to use ##
+
+1. Create a configuration for NFS rootfs creation process (see [`machine-name.conf.example`] for an example)
+
+2. Start the creation process (you need to be `root`!):
+   ```
+   # /path/to/build-nfs-rootfs machine-name.conf
+   Now downloading files...
+   [...]
+   Checking validity of files with sha256...
+   `base56.tgz' is valid.
+   `etc56.tgz' is valid.
+   `man56.tgz' is valid.
+   `bsd.mp' is valid.
+   Finished.
+   Creating swap file
+   Finished.
+   Now extracting base56.tgz... OK
+   Now extracting etc56.tgz... OK
+   Now extracting man56.tgz... OK
+   Now configuring file system... 
+   Creating devices... OK
+   Creating /etc/fstab... OK
+   Creating /etc/myname... OK
+   Copying /etc/hosts from host... OK
+   Creating /etc/hostname.[...]... OK
+   Installing kernel... OK
+   Placing OpenBSD version number in `/[...]/machine-name/root/etc/openbsd_version'... OK
+   ```
+   > **NOTICE:** For target and host OpenBSD versions since 5.5 file validity can be checked with [`signify`]. The builder uses `signify` on OpenBSD 5.5 and greater and `sha256` on OpenBSD 5.4 and smaller.
+
+3. Precreate OpenSSL/OpenSSH keys for the target system (optional!)
+   ```
+   # /root/bin/generate-keys /machine-name/root/
+   Warning: Host OS version is smaller than target OS version. Using available SSH key types of host OS only.
+   openssl: generating isakmpd/iked RSA key... OK
+   ssh-keygen: generating openssh keys... rsa1 dsa ecdsa rsa OK
+   ```
+   > **NOTICE:** The key generation on the host comes in handy for slow target machines (e.g. SUN SPARCstation 10) which need a considerable amount of time to create SSH keys. If your host OS is older than the target OS, you can still create the SSH key types that are available on your host OS. On first run the target machine will create the missing keys.
+
+## License ##
+
+(GPLv3)
+
+Copyright (C) 2014 Frank Scheiner
+
+The software is distributed under the terms of the GNU General Public License
+
+This software is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This software is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a [copy] of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+[copy]: /COPYING
+[`machine-name.conf.example`]: /share/doc/machine-name.conf.example
+[`signify`]: http://www.openbsd.org/cgi-bin/man.cgi/OpenBSD-5.5/man1/signify.1?query=signify&manpath=OpenBSD-5.5
+
---
+++ bin/build-nfs-rootfs.bash
@@ -0,0 +1,316 @@
+#!/usr/bin/env bash
+
+# build NFS root file system for OpenBSD
+
+:<<COPYRIGHT
+
+Copyright (C) 2014 Frank Scheiner
+
+The program is distributed under the terms of the GNU General Public License
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+COPYRIGHT
+
+################################################################################
+# DEFINES
+################################################################################
+
+readonly _program="build-nfs-root"
+
+readonly _version="0.3.0"
+
+readonly _exit_usage=64
+
+readonly _true=1
+readonly _false=0
+
+################################################################################
+# FUNCTIONS
+################################################################################
+
+usageMsg()
+{
+	cat >&2 <<-USAGE
+	Usage: $_program "configurationFile"
+	USAGE
+	
+	return
+}
+
+
+isValidFileForSignify()
+{
+	local _openbsdVersionNonDotted="$1"
+	local _signatureFile="$2"
+	local _file="$3"
+	
+	if signify -C -p "/etc/signify/openbsd-${_openbsdVersionNonDotted}-base.pub" \
+	           -x "$_signatureFile" "$_file" &>/dev/null; then
+	
+		return 0
+	else
+		return 1
+	fi
+}
+
+
+isValidFileForSha256()
+{
+	local _hashFile="$1"
+	local _file="$2"
+	
+	local _tempHashFile=$( mktemp )
+	
+	grep "($_file)" "$_hashFile" > "$_tempHashFile"
+	
+	if sha256 -c "$_tempHashFile" &>/dev/null; then
+
+		rm "$_tempHashFile"
+
+		return 0
+	else
+		rm "$_tempHashFile"
+
+		return 1
+	fi		
+}
+
+
+downloadFileWithWget()
+{
+	local _file="$1"
+	
+	#wget -q -c "$_file"
+	wget -4 --progress=bar -c "$_file"
+	
+	return
+}
+
+
+downloadFile()
+{
+	downloadFileWithWget $@
+}
+
+
+################################################################################
+# MAIN
+################################################################################
+
+if [[ $1 == "" ]]; then
+	usageMsg
+	exit $_exit_usage
+fi
+
+_configurationFile="$1"
+
+. "$_configurationFile"
+
+
+_openbsdVersionNonDotted=${_openbsdVersion/./} # "55"
+
+# Minimum OpenBSD version with signify support for downloads
+_minimumOpenbsdVersionNonDotted="55"
+
+# Signature file name
+_signatureFile="SHA256.sig"
+
+# File containing the hashes
+_hashFile="SHA256"
+
+
+_downloadBasePath="${_downloadMirror}/${_openbsdVersion}/${_openbsdPlatform}"
+
+# Save files in the super directory
+mkdir -p "$_basePath" && cd "$_basePath/.."
+
+echo "Now downloading files..."
+
+for _set in "${_distributionSetsToDownload[@]}"; do
+
+	_fileName="${_set}${_openbsdVersionNonDotted}.tgz"
+	
+	_file="${_downloadBasePath}/${_fileName}"
+
+	# Always download files. Let the downloader detect if file was
+	# downloaded completely.
+	#if [[ ! -e "$_fileName" ]]; then
+
+		downloadFile "$_file"
+	#fi
+done
+
+# Always download files. Let the downloader detect if file was downloaded
+# completely.
+#if [[ ! -e "$_kernelToUse" ]]; then
+	downloadFile "${_downloadBasePath}/${_kernelToUse}"
+#fi
+
+#echo "Now downloading files..."
+
+#wait
+
+echo "Finished."
+
+# Perform validity and signature test only, if files are from OpenBSD 5.5 or
+# newer and if the signify tool is available.
+if test $_openbsdVersionNonDotted -ge $_minimumOpenbsdVersionNonDotted && \
+   which signify &>/dev/null; then
+
+	downloadFile "${_downloadBasePath}/${_signatureFile}"
+
+	_invalidFiles=$false
+
+	echo "Checking validity of files with signify..."
+	for _set in "${_distributionSetsToDownload[@]}"; do
+	
+		_fileName="${_set}${_openbsdVersionNonDotted}.tgz"
+
+		_file="${_downloadBasePath}/${_fileName}"
+		
+		# check validity
+		if ! isValidFileForSignify "$_openbsdVersionNonDotted" "$_signatureFile" "$_fileName"; then
+			echo "\`${_fileName}' is invalid."
+			_invalidFiles=$true
+		else
+			echo "\`${_fileName}' is valid."
+		fi
+	done
+	
+	if ! isValidFileForSignify "$_openbsdVersionNonDotted" "$_signatureFile" "$_kernelToUse"; then
+		echo "\`${_kernelToUse}' is invalid."
+		_invalidFiles=$true
+	else
+		echo "\`${_kernelToUse}' is valid."
+	fi
+	
+	echo "Finished."
+	
+	if [[ $_invalidFiles -eq $_true ]]; then
+		echo "Detected invalid files. Cannot continue. Please delete invalid file(s) and try again."
+		exit 1
+	fi
+# Just do validity test
+else
+	downloadFile "${_downloadBasePath}/${_hashFile}"
+	
+	_invalidFiles=$false
+	
+	echo "Checking validity of files with sha256..."
+	for _set in "${_distributionSetsToDownload[@]}"; do
+	
+		_fileName="${_set}${_openbsdVersionNonDotted}.tgz"
+
+		_file="${_downloadBasePath}/${_fileName}"
+		
+		# check validity
+		if ! isValidFileForSha256 "$_hashFile" "$_fileName"; then
+			echo "\`${_fileName}' is invalid."
+			_invalidFiles=$_true
+		else
+			echo "\`${_fileName}' is valid."
+		fi
+	done
+	
+	if ! isValidFileForSha256 "$_hashFile" "$_kernelToUse"; then
+		echo "\`${_kernelToUse}' is invalid."
+		_invalidFiles=$_true
+	else
+		echo "\`${_kernelToUse}' is valid."
+	fi
+	
+	echo "Finished."
+	
+	if [[ $_invalidFiles -eq $_true ]]; then
+		echo "Detected invalid files. Cannot continue. Please delete invalid file(s) and try again."
+		exit 1
+	fi
+fi
+
+# Return to base path
+cd "$_basePath"
+
+#echo "=> $PWD"
+
+echo "Creating swap file"
+dd if=/dev/zero of=swap bs=1M seek=128 count=0 2>/dev/null
+echo "Finished."
+
+mkdir -p "root" && cd "root"
+for _set in "${_distributionSetsToDownload[@]}"; do
+	echo -n "Now extracting ${_set}${_openbsdVersionNonDotted}.tgz... "
+	tar -xzpf ../../${_set}${_openbsdVersionNonDotted}.tgz
+	echo "OK"
+done
+cd ..
+
+
+_rootOfFileSystem="$PWD/root"
+
+cd "$_rootOfFileSystem"
+
+echo "Now configuring file system... "
+
+# Configure file system
+mkdir -p swap
+
+
+echo -n "Creating devices... "
+
+cd "dev"
+./MAKEDEV all
+echo "OK"
+
+
+cd "$_rootOfFileSystem"
+
+
+echo -n "Creating /etc/fstab... "
+cat > etc/fstab <<-EOF
+${_nfsServerAddress}:${_basePath}/root / nfs rw,tcp,nfsv3 0 0
+${_nfsServerAddress}:${_basePath}/swap none swap sw,nfsmntpt=/swap,tcp
+EOF
+echo "OK"
+
+
+echo -n "Creating /etc/myname... "
+echo "${_hostname}.${_domain}" > etc/myname
+echo "OK"
+
+
+echo -n "Copying /etc/hosts from host... "
+cp /etc/hosts etc/hosts
+echo "OK"
+
+
+echo -n "Creating /etc/hostname.[...]... "
+echo "inet $_ipAddress" > "etc/hostname.${_bootNetworkInterface}"
+chmod 0640 "etc/hostname.${_bootNetworkInterface}"
+echo "OK"
+
+
+echo -n "Installing kernel... "
+cp ../../${_kernelToUse} .
+if [[ "${_kernelToUse}" != "bsd" ]]; then
+	ln -s ${_kernelToUse} bsd
+fi
+echo "OK"
+
+echo -n "Placing OpenBSD version number in \`${_rootOfFileSystem}/etc/openbsd_version'... "
+echo "$_openbsdVersion" > "$_rootOfFileSystem/etc/openbsd_version"
+echo "OK"
+
+exit
+
